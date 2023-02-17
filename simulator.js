c60b66bea6ede7a2810ed0ad39787fcc49fd180b
Rsaenz <rsaenz0791@gmail.com>
2023-02-17 16:34:10 -0600

    js ppe1

--- simulator.js
+++ simulator.js
@@ -1,7 +1,22 @@
-let año_actual = parseInt(prompt("Ingrese el año actual"))
-let año_nacimiento = parseInt(prompt("Ingrese su año de nacimiento"))
+function agradecer() {
+    let agradecimiento = "Gracias por la info"
+    alert(agradecimiento) 
+    
+}
+  
+
+let nombre = prompt("Ingrese su nombre");
+agradecer();
+let año_actual = parseInt(prompt("Ingrese el año actual"));
+agradecer();
+let año_nacimiento = parseInt(prompt("Ingrese su año de nacimiento"));
+agradecer();
 let resultado = año_actual - año_nacimiento;
+
+alert( "Hola " + nombre );
+
 alert("Usted tiene " + resultado + " años ");
+
 if (resultado >= 18 ){
     alert("Usted es mayor de edad");
 }
@@ -9,5 +24,5 @@ if (resultado <= 18 ){
     alert("Usted es menor de edad");
 }
 if (resultado >= 30){
-    alert("Usted ya esta muy viejo XD")
+    alert("Usted ya esta muy viejo XD");
 }
