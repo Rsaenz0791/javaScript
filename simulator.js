2346f4ad7c8cc1e3650ddddea56537867b55cdc9
Rsaenz <rsaenz0791@gmail.com>
2023-03-06 16:38:08 -0600

    preentrega2

--- simulator.js
+++ simulator.js
@@ -26,3 +26,23 @@ if (resultado <= 18 ){
 if (resultado >= 30){
     alert("Usted ya esta muy viejo XD");
 }
+
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+async function copyText(e) {
+    //evita que el boton se vaya al sitio
+        e.preventDefault()
+        const link = this.getAttribute('link')
+        console.log(link)
+        try {
+            await navigator.clipboard.writeText(link)
+            alert("Se copio el link: " + link)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+
+shareButtons.forEach(shareButton =>
+        shareButton.addEventListener('click', copyText))
